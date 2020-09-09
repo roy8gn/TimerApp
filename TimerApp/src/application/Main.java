2659fb174bf3a40e34e0663307ff0c5de1f3263e
Roy <roy@gmail.com>
2020-09-09 13:26:41 +0300

    Dark/Light mode added.

--- TimerApp/src/application/Main.java
+++ TimerApp/src/application/Main.java
@@ -3,20 +3,33 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	
+	static Scene timerScene;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("TimerGUI.fxml"));
-			BorderPane root = loader.load();
+			/*FXMLLoader loader = new FXMLLoader(getClass().getResource("TimerGUI.fxml"));
+			Parent root = loader.load();
 			Scene scene = new Scene(root,505,367);
 			scene.getStylesheets().add(getClass().getResource("light.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.show();*/
+			
+			Parent root = FXMLLoader.load(getClass().getResource("TimerGUI.fxml"));
+			timerScene = new Scene(root,505,367);
+			
+			timerScene.getStylesheets().add(getClass().getResource("light.css").toExternalForm());
+			primaryStage.setTitle("TimerApp");
+			primaryStage.setScene(timerScene);
 			primaryStage.show();
+	        
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- TimerApp/src/application/TimerController.java
+++ TimerApp/src/application/TimerController.java
@@ -1,12 +1,29 @@
 package application;
 
+
+import java.io.File;
+import java.net.URL;
+import java.util.ConcurrentModificationException;
+import java.util.ResourceBundle;
+
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+import javafx.scene.media.MediaPlayer;
+
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 
-public class TimerController {
+public class TimerController implements Initializable {
 
     @FXML
     private TextField note;
@@ -18,7 +35,7 @@ public class TimerController {
     private TextField seconds;
 
     @FXML
-    private TextField hour;
+    private TextField hours;
 
     @FXML
     private Button startButton;
@@ -27,34 +44,191 @@ public class TimerController {
     private TextField minutes;
 
     @FXML
-    private ProgressBar progressionBar;
+    private ProgressBar timerProgressionBar;
 
+    @FXML
+    private ProgressIndicator TimerIndicator;
+    
     @FXML
     private Button darkButton;
 
     @FXML
     private Button lightButton;
 
+    private boolean isPlaying;
+    private boolean isRunning;
+    private TimerController.Timer timer;
+    private int secondesToRun;
+    private MediaPlayer mediaPlayer;
+    private Alert alert;
+    private Alert wrongInputAlert;
+
+    @Override
+	public void initialize(URL location, ResourceBundle resources) { // Initialize Controller
+		isRunning = false;
+		isPlaying = false;
+		lightButton.setDisable(true);
+    	darkButton.setDisable(false);
+    	
+    	alert = new Alert(AlertType.INFORMATION);
+    	alert.setTitle("TimerApp Reminder");
+    	alert.setHeaderText("Time's Up.");
+    	
+    	wrongInputAlert = new Alert(AlertType.ERROR);
+    	wrongInputAlert.setTitle("TimerApp Reminder");
+    	wrongInputAlert.setHeaderText("Invalid Input");
+    	wrongInputAlert.setContentText("Input Must be only numbers!\n"
+    			+ "Hours allowed range: 0-99\n"
+    			+ "Minutes and Seconds Allowed range: 0-59");
+    	try {
+			String mp3 = "pokemon_route_1.mp3";
+			URL resource = getClass().getResource(mp3);
+			System.out.println(resource.toString());
+			Media media = new Media(resource.toString());
+			mediaPlayer = new MediaPlayer(media);
+		}
+		catch(MediaException ex) {
+			System.out.println("ERROR while locating the alarm sound.");
+		}
+	}
+    
     @FXML
     void darkMode(ActionEvent event) {
     	lightButton.setDisable(false);
     	darkButton.setDisable(true);
+    	Main.timerScene.getStylesheets().clear();
+    	Main.timerScene.getStylesheets().add(getClass().getResource("dark.css").toExternalForm());
     }
 
     @FXML
     void lightMode(ActionEvent event) {
     	lightButton.setDisable(true);
     	darkButton.setDisable(false);
+    	Main.timerScene.getStylesheets().clear();
+    	Main.timerScene.getStylesheets().add(getClass().getResource("light.css").toExternalForm());
     }
 
     @FXML
-    void startTimer(ActionEvent event) {
+    void startTimer(ActionEvent event) throws InterruptedException {
+    	
+    	if(!(onlyNumbers(hours, 99) && onlyNumbers(minutes, 59) && onlyNumbers(seconds, 59))){
+    		wrongInputAlert.show();
+    		return;
+    	}
+    	secondesToRun = getInt(seconds) + getInt(minutes)*60 + getInt(hours)*60*60;
 
+    	hours.setEditable(false);
+    	minutes.setEditable(false);
+    	seconds.setEditable(false);
+    	note.setEditable(false);
+    	timer = new Timer(secondesToRun);
+    	System.out.println(secondesToRun);
+    	isRunning = true;
+    	
+    	try {
+    		timer.start();
+    	}
+		catch(ConcurrentModificationException ex){}
+		
+    	
+    }
+    
+    void releaseTimer() { // Reset Button is pressed
+    	hours.setEditable(true);
+    	minutes.setEditable(true);
+    	seconds.setEditable(true);
+    	note.setEditable(true);
+    	isRunning = false;
+    	mediaPlayer.stop();
     }
 
     @FXML
     void resetTimer(ActionEvent event) {
-
+    	
+    	hours.setText("00");
+    	minutes.setText("00");
+    	seconds.setText("00");
+    	note.clear();
+    	releaseTimer();
+    	isPlaying = false;
+    	timerProgressionBar.setProgress(0);
+    	TimerIndicator.setProgress(0);
     }
+	
+	public void setTextForTextfield(TextField tf, int time) {
+		if(time<10) {
+			tf.setText("0"+time);
+		}
+		else {
+			tf.setText(""+time);
+		}
+	}
+	
+	
+	public int getInt(TextField tf) { // get the int value from a TextField
+		 return Integer.parseInt(tf.getText().toString());
+	}
+	
+	public boolean onlyNumbers(TextField tf, int max) {
+		try {
+		     int number = getInt(tf);
+		     if(number>max) return false;
+		}
+		catch (NumberFormatException e) {
+		     return false;
+		}
+		return true;
+	}
+	
+	
+	public class Timer extends Thread{
 
+		private int secondsCount;
+		private int minutesCount;
+		private int hoursCount;
+		private int originalNumberOfSeconds;
+		public Timer(int seconds) {
+			originalNumberOfSeconds = seconds;
+		}
+		
+		@Override @FXML
+		public void run() throws ConcurrentModificationException {
+			double precentage;
+			while(isRunning && secondesToRun>=0) {
+				secondsCount = secondesToRun%60;
+				minutesCount = (secondesToRun/60)%60; //getInt(minutes);
+				hoursCount = secondesToRun/60/60;//getInt(hours);
+				precentage = 1.0-((double)secondesToRun/(double)originalNumberOfSeconds);
+				timerProgressionBar.setProgress(precentage);
+				TimerIndicator.setProgress(precentage);
+				
+				setTextForTextfield(seconds, secondsCount);
+				setTextForTextfield(minutes, minutesCount);
+				setTextForTextfield(hours, hoursCount);
+				secondesToRun--;
+				System.out.println(secondesToRun);
+				try {
+					sleep(1000);
+				} catch (InterruptedException e) {
+					System.out.println("ERROR");
+				}
+			}
+			
+			if(secondesToRun<1) {
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						if(note.getText().toString().equals("")) {
+							alert.setContentText("Time's Up.");
+						}
+						else {
+							alert.setContentText(note.getText().toString());
+						}
+						mediaPlayer.play();
+						alert.show();
+					}
+				});
+			}
+		}
+	}
 }
